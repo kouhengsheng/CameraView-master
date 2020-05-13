0007484cc5e81e6c2fa5164fa9be33c6a77548a9
kouhengsheng <hengsheng.kou@yintech.cn>
2020-05-13 18:44:48 +0800

    Initial commit

---
+++ cameraapplication/src/main/java/com/zxing/cameraapplication/CameraApplication.java
@@ -0,0 +1,20 @@
+package com.zxing.cameraapplication;
+
+import android.app.Application;
+
+
+/**
+ * =====================================
+ * 作    者: 陈嘉桐
+ * 版    本：1.1.4
+ * 创建日期：2017/5/8
+ * 描    述：
+ * =====================================
+ */
+public class CameraApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
